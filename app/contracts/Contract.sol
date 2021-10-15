ac6c564af6be349dd66321ffbeb87d2752978f14
Kevin Kong <200sxmeda@gmail.com>
2021-10-16 10:50:42 +1100

    Add mapping to keep track of individual wave counts

--- app/contracts/Contract.sol
+++ app/contracts/Contract.sol
@@ -6,6 +6,7 @@ import "hardhat/console.sol";
 
 contract Contract {
     uint256 totalWaves; //contract state variable, auto initalized to 0. 
+    mapping (address => uint256) public numWaves; // initialized where  every possible Ethereum address exists in the mapping and is mapped to a corresponding level of 0
 
     constructor() {
         console.log("constructing contract");
@@ -13,11 +14,13 @@ contract Contract {
 
     function wave() public { // public keyword -> function will be available to be called on the blockchain
         totalWaves += 1; 
-        console.log("%s has waved!", msg.sender); // msg.sender is the wallet address of the person who called the function
+        numWaves[msg.sender] += 1; 
+        console.log("%s has waved %d times!", msg.sender, numWaves[msg.sender]); // msg.sender is the wallet address of the person who called the function
     }
 
     function getTotalWaves() public view returns (uint256) {
         console.log("We have %d total waves!", totalWaves); 
         return totalWaves; 
     }
+
 }
--- app/scripts/run.js
+++ app/scripts/run.js
@@ -6,14 +6,25 @@ const main = async () => {
 
     console.log("Contract is deployed to:", contract.address);
     console.log("Contract deployed by:", owner.address);
+    console.log("Random person's address is:", randomPerson.address); 
 
+    // getTotalWaves, wave once and then getTotalWaves again
     let waveCount;
     waveCount = await contract.getTotalWaves(); // awaiting even though contract function not async? 
-
     let waveTxn = await contract.wave(); 
     await waveTxn.wait(); 
-
     waveCount = await contract.getTotalWaves(); 
+
+    // connect to contract with randomPerson, wave, getTotalWaves
+    waveTxn = await contract.connect(randomPerson).wave();
+    await waveTxn.wait();
+    waveCount = await contract.getTotalWaves();    
+
+    // get owner to wave once more and getTotalWaves again 
+    waveTxn = await contract.wave(); 
+    await waveTxn.wait(); 
+    waveCount = await contract.getTotalWaves();    
+
   };
   
   const runMain = async () => {
