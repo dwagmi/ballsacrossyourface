215b1b01a24d5273e084d7b7d28eaf0f45709917
Kevin Kong <200sxmeda@gmail.com>
2021-10-16 10:14:11 +1100

    Add contract functions to persist state

--- app/contracts/Contract.sol
+++ app/contracts/Contract.sol
@@ -5,7 +5,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Contract {
+    uint256 totalWaves; //contract state variable, auto initalized to 0. 
+
     constructor() {
         console.log("constructing contract");
+    }
+
+    function wave() public {
+        totalWaves += 1; 
+        console.log("%s has waved!", msg.sender); // msg.sender is the wallet address of the person who called the function
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves); 
+        return totalWaves; 
     }
 }
