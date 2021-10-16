b9aed476cfcc0597de11f8b649bff523fd4b813a
Kevin Kong <200sxmeda@gmail.com>
2021-10-16 16:18:41 +1100

    Change cooldown to 30s, finish

--- app/contracts/Contract.sol
+++ app/contracts/Contract.sol
@@ -7,12 +7,14 @@ import "hardhat/console.sol";
 contract Contract {
     uint256 totalWaves; //contract state variable, auto initalized to 0. 
     mapping (address => uint256) public numWaves; // initialized where  every possible Ethereum address exists in the mapping and is mapped to a corresponding level of 0
-
+    /*
+     * We will be using this below to help generate a random number
+     */
+    uint256 private seed;
     /*
      * A little magic, Google what events are in Solidity!
      */
     event NewWave(address indexed from, uint256 timestamp, string message);
-
     /*
      * I created a struct here named Wave.
      * A struct is basically a custom datatype where we can customize what we want to hold inside it.
@@ -22,40 +24,63 @@ contract Contract {
         string message; // The message the user sent.
         uint256 timestamp; // The timestamp when the user waved.
     }
-
     /*
      * I declare a variable waves that lets me store an array of structs.
      * This is what lets me hold all the waves anyone ever sends to me!
      */
     Wave[] waves;
+    /*
+     * This is an address => uint mapping, meaning I can associate an address with a number!
+     * In this case, I'll be storing the address with the last time the user waved at us.
+     */
+    mapping(address => uint256) public lastWavedAt;
+
 
     constructor() payable {
         console.log("CONSTRUCTING SMART CONTRACT. POG.");
     }
 
-
-
     function wave(string memory _message) public { // public keyword -> function will be available to be called on the blockchain
+        /*
+         * We need to make sure the current timestamp is at least 15-minutes bigger than the last timestamp we stored
+         */
+        require(
+            lastWavedAt[msg.sender] + 30 seconds < block.timestamp,
+            "Wait 30s"
+        );
+        /*
+         * Update the current timestamp we have for the user
+         */
+        lastWavedAt[msg.sender] = block.timestamp;
+
         totalWaves += 1; 
         numWaves[msg.sender] += 1; 
         console.log("%s has waved %d times!", msg.sender, numWaves[msg.sender]); // msg.sender is the wallet address of the person who called the function
-
         waves.push(Wave(msg.sender, _message, block.timestamp));
-
         /*
-         * I added some fanciness here, Google it and try to figure out what it is!
-         * Let me know what you learn in #general-chill-chat
+         * Generate a Psuedo random number between 0 and 100
          */
-        emit NewWave(msg.sender, block.timestamp, _message);        
-
-        // Send eth to users who waved 
-         uint256 prizeAmount = 0.0001 ether;
-        require(
-            prizeAmount <= address(this).balance,
-            "Trying to withdraw more money than the contract has."
-        );
-        (bool success, ) = (msg.sender).call{value: prizeAmount}("");
-        require(success, "Failed to withdraw money from contract.");
+        uint256 randomNumber = (block.difficulty + block.timestamp + seed) % 100;
+        console.log("Random # generated: %s", randomNumber);
+        /*
+         * Set the generated, random number as the seed for the next wave
+         */
+        seed = randomNumber;
+        /*
+         * Give a 50% chance that the user wins the prize.
+         */
+        if (randomNumber < 50) {
+            console.log("%s won!", msg.sender);
+            // Send eth to users who waved 
+            uint256 prizeAmount = 0.0001 ether;
+            require(
+                prizeAmount <= address(this).balance,
+                "Trying to withdraw more money than the contract has."
+            );
+            (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+            require(success, "Failed to withdraw money from contract.");
+        }
+        emit NewWave(msg.sender, block.timestamp, _message); // real time 'stream?' updates       
     }
 
     /*
