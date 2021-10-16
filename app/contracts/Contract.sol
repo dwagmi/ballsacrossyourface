e36cb5c50b8a77d0cdc824daea5542ad4253fe02
Kevin Kong <200sxmeda@gmail.com>
2021-10-16 15:22:00 +1100

    Add messaging and timestamp functionality to Contract

--- app/contracts/Contract.sol
+++ app/contracts/Contract.sol
@@ -8,14 +8,53 @@ contract Contract {
     uint256 totalWaves; //contract state variable, auto initalized to 0. 
     mapping (address => uint256) public numWaves; // initialized where  every possible Ethereum address exists in the mapping and is mapped to a corresponding level of 0
 
-    constructor() {
-        console.log("constructing contract");
+    /*
+     * A little magic, Google what events are in Solidity!
+     */
+    event NewWave(address indexed from, uint256 timestamp, string message);
+
+    /*
+     * I created a struct here named Wave.
+     * A struct is basically a custom datatype where we can customize what we want to hold inside it.
+     */
+    struct Wave {
+        address waver; // The address of the user who waved.
+        string message; // The message the user sent.
+        uint256 timestamp; // The timestamp when the user waved.
     }
 
-    function wave() public { // public keyword -> function will be available to be called on the blockchain
+    /*
+     * I declare a variable waves that lets me store an array of structs.
+     * This is what lets me hold all the waves anyone ever sends to me!
+     */
+    Wave[] waves;
+
+    constructor() {
+        console.log("CONSTRUCTING SMART CONTRACT. POG.");
+    }
+
+
+
+    function wave(string memory _message) public { // public keyword -> function will be available to be called on the blockchain
         totalWaves += 1; 
         numWaves[msg.sender] += 1; 
         console.log("%s has waved %d times!", msg.sender, numWaves[msg.sender]); // msg.sender is the wallet address of the person who called the function
+
+        waves.push(Wave(msg.sender, _message, block.timestamp));
+
+        /*
+         * I added some fanciness here, Google it and try to figure out what it is!
+         * Let me know what you learn in #general-chill-chat
+         */
+        emit NewWave(msg.sender, block.timestamp, _message);        
+    }
+
+    /*
+     * I added a function getAllWaves which will return the struct array, waves, to us.
+     * This will make it easy to retrieve the waves from our website!
+     */
+    function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
--- app/scripts/run.js
+++ app/scripts/run.js
@@ -1,40 +1,35 @@
 const main = async () => {
-  const [owner, randomPerson] = await hre.ethers.getSigners(); // get generated wallet addresses for contract owner and randomPerson from hardhat
-    const contractFactory = await hre.ethers.getContractFactory('Contract');
-    const contract = await contractFactory.deploy();
-    await contract.deployed();
+  const waveContractFactory = await hre.ethers.getContractFactory('Contract');
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log('Contract addy:', waveContract.address);
 
-    console.log("Contract is deployed to:", contract.address);
-    console.log("Contract deployed by:", owner.address);
-    console.log("Random person's address is:", randomPerson.address); 
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
 
-    // getTotalWaves, wave once and then getTotalWaves again
-    let waveCount;
-    waveCount = await contract.getTotalWaves(); // awaiting even though contract function not async? 
-    let waveTxn = await contract.wave(); 
-    await waveTxn.wait(); 
-    waveCount = await contract.getTotalWaves(); 
+  /**
+   * Let's send a few waves!
+   */
+  let waveTxn = await waveContract.wave('A message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-    // connect to contract with randomPerson, wave, getTotalWaves
-    waveTxn = await contract.connect(randomPerson).wave();
-    await waveTxn.wait();
-    waveCount = await contract.getTotalWaves();    
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
+  await waveTxn.wait(); // Wait for the transaction to be mined
 
-    // get owner to wave once more and getTotalWaves again 
-    waveTxn = await contract.wave(); 
-    await waveTxn.wait(); 
-    waveCount = await contract.getTotalWaves();    
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
 
-  };
-  
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
